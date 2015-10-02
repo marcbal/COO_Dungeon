0452b89ec327c33588cbc1599cf7d32258203fe8
Kaisow <kaisow@live.fr>
2015-10-02 17:37:49 +0200

    Add monster tests

---
+++ src/fr/univ_lille1/fil/coo/dungeon_tests/MonsterTest.java
@@ -0,0 +1,14 @@
+package fr.univ_lille1.fil.coo.dungeon_tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MonsterTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
